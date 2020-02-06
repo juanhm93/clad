f4def5b97643ac7fa5748ffe8c03859f2cad4280
Juan Hernandez <juanhm93@gmail.com>
2020-02-06 14:53:42 -0400

    cambios de botones de redes sociales en el footer agregando marca de agua a los post de las noticias en el lado derecho

--- slideshow.php
+++ slideshow.php
@@ -1,4 +1,12 @@
-       <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+     <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+       <ol class="carousel-indicators">
+       <?php query_posts('category_name=slider'); ?>
+       <?php 
+            $j = 0;
+            if(have_posts() ) : while (have_posts() ): the_post(); ?>
+          <li data-target="#carouselExampleCaptions" data-slide-to="<?php $j ?>" <?php if($j == 0) echo 'class=active'?>></li>
+          <?php $j++; endwhile; endif; ?> 
+       </ol>
           <div class="carousel-inner">
             <?php query_posts('category_name=slider'); ?>
             <?php 
@@ -7,15 +15,19 @@
             <div class="carousel-item <?php if($i == 1) echo 'active'  ?>">
              <a href="<?php the_permalink(); ?>"><?php if ( has_post_thumbnail() ) {
             the_post_thumbnail('large', array( 'class' => 'd-block w-100' ));
-              } ?></a> 
+              } ?></a>
+              <div class="carousel-caption d-none d-md-block">
+                <h4><?php the_title() ?></h4>
+                <p><?php the_excerpt();  ?></p>      
+              </div> 
             </div>
             <?php $i++; endwhile; endif; ?>
           </div>
-          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="sr-only">Previous</span>
           </a>
-          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
           </a>
